2cd1ab2666133d9b8a813e0fcbf950815f3236f3
olayemi odefunsho <odefunsho.olayemi@gmail.com>
2020-04-03 23:13:32 +0100

    add test project to acm.bl

--- ACM.BL/Class1.cs
+++ ACM.BL/Class1.cs
@@ -2,7 +2,27 @@
 
 namespace ACM.BL
 {
-    public class Class1
+    public class Customer
     {
+        public int CustomerId { get; private set; }
+        public string EmailAddress { get; set; }
+        public string FirstName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                return LastName + ',' + FirstName;
+            }
+
+        }
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+        
+        
     }
 }
